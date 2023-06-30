7d97f8268e2f7f7bb020bf42db571ae38a400fab
shubhamkulkarni95 <shu.sun.k@gmail.com>
2023-06-30 19:28:45 +0530

    Code with report

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,11 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
     },
     specPattern:'cypress/e2e/**.js',
     baseUrl : "https://restful-booker.herokuapp.com/"
